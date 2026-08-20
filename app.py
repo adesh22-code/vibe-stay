bc2fa9d8a8268bccca7bf056facab88407a57088
adesh22-code <144451382+adesh22-code@users.noreply.github.com>
2026-08-20 21:21:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+
+from services.github_service import get_data
+
 
 app = Flask(__name__)
 
@@ -6,6 +9,12 @@ app = Flask(__name__)
 @app.route("/")
 def admin():
     return render_template("admin.html")
+
+
+@app.route("/api/homestays")
+def homestays():
+    data = get_data()
+    return jsonify(data)
 
 
 if __name__ == "__main__":
